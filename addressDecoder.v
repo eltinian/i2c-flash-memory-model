d352c2c523611c740ed8e63d57baefc8b361e618
eltinian <eltinian@users.noreply.github.com>
2016-04-18 23:30:34 -0700

    Update addressDecoder.v

--- addressDecoder.v
+++ addressDecoder.v
@@ -1,17 +1,17 @@
-module add_decoder(addrBank, addrBlock, addrRow, adAddrIn, enable);
+module add_decoder(addrBank, addrBlock, addrRow, addrIn, enable);
 
 output reg [3:0] addrBank, addrBlock;
 output reg [7:0] addrRow;
-input wire [15:0] adAddrIn;
+input wire [15:0] addrIn;
 input wire enable;
 
-always@(enable or adAddrIn)
+always@(enable or addrIn)
 begin
 	if(!enable)
 	begin
-		addrBank = adAddrIn[15:12];
-		addrBlock = adAddrIn[11:8];
-		addrRow = adAddrIn[7:0];
+		addrBank = addrIn[15:12];
+		addrBlock = addrIn[11:8];
+		addrRow = addrIn[7:0];
 	end
 end
 
